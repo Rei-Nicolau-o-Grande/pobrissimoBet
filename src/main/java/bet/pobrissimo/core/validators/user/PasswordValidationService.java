770c055234ff85f9a7539967a78dabde6a121f7f
Jack Sparrow <nicolasmeireles1004@gmail.com>
2024-09-09 12:03:00 -0300

    :poop: feat: validate password contains lower case.

--- src/main/java/bet/pobrissimo/core/validators/user/PasswordValidationService.java
+++ src/main/java/bet/pobrissimo/core/validators/user/PasswordValidationService.java
@@ -14,14 +14,15 @@ public class PasswordValidationService implements ValidationContract {
     public void validate(String password, Map<String, List<String>> errorFields) {
         validateContainsNumber(password, errorFields);
         validateContainsUpperCase(password, errorFields);
-        validateLengthMin3(password, errorFields);
+        validateContainsLowerCase(password, errorFields);
+        validateLengthMin6(password, errorFields);
 
     }
 
-    public static void validateLengthMin3(String password, Map<String, List<String>> errorFields) {
-        if (password.length() < 3) {
+    public static void validateLengthMin6(String password, Map<String, List<String>> errorFields) {
+        if (password.length() < 6) {
             errorFields.computeIfAbsent("password", k -> new ArrayList<>())
-                    .add("A senha deve ter no mínimo 3 caracteres.");
+                    .add("A senha deve ter no mínimo 6 caracteres.");
         }
     }
 
@@ -29,6 +30,13 @@ public class PasswordValidationService implements ValidationContract {
         if (password.chars().noneMatch(Character::isUpperCase)) {
             errorFields.computeIfAbsent("password", k -> new ArrayList<>())
                     .add("A senha deve conter pelo menos uma letra maiúscula.");
+        }
+    }
+
+    public static void validateContainsLowerCase(String password, Map<String, List<String>> errorFields) {
+        if (password.chars().noneMatch(Character::isLowerCase)) {
+            errorFields.computeIfAbsent("password", k -> new ArrayList<>())
+                    .add("A senha deve conter pelo menos uma letra minúscula.");
         }
     }
 
