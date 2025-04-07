0d68371e36b88ec3106f1ca3ea1de2f728d4199a
Jack Sparrow <nicolasmeireles1004@gmail.com>
2025-04-07 00:57:51 -0300

    :poop: feat: create validate notnull transaction .

--- src/main/java/bet/pobrissimo/dtos/transaction/TransactionRequestDto.java
+++ src/main/java/bet/pobrissimo/dtos/transaction/TransactionRequestDto.java
@@ -2,11 +2,13 @@ package bet.pobrissimo.dtos.transaction;
 
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record TransactionRequestDto(
 
+        @NotNull(message = "O valor não pode ser nulo.")
         @DecimalMin(value = "0.01", message = "O valor da transação deve ser maior que zero.")
         @DecimalMax(value = "1000000.00", message = "O valor da transação não pode ser maior que um milhão.")
         BigDecimal value) {
